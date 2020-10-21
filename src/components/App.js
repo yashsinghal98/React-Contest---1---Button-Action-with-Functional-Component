f593089bafc8a060f56a7be49d1a77c59aa85553
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-21 20:19:37 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [click,setclick]=React.useState(false);
+  let handleclick=()=>{
+    let val=click;
+    val=true;
+    setclick(val);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={()=>handleclick()}></button>
+      {click&&(<pre id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</pre>)}
     </div>
   );
 }
